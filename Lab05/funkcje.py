8936dbb79f1f39b79b7caf17e3524f02b6a4bb41
Iza <lilamovie@wp.pl>
2023-04-12 09:43:05 +0200

    lab05

--- Lab05/funkcje.py
+++ Lab05/funkcje.py
@@ -99,3 +99,85 @@ wypisz3(1, 2)
 wypisz3(1, 2, 3, e=5, f=6, g=7, h=8, i=9, j=10)
 
 
+# przekazywanie argumentow za pomoca slowa kluczowego
+def wypisz4(a, b, *args, c):
+    print(a, b, *args, c)
+
+
+wypisz4(1, 2, 3, 4, 5, c=6)
+
+"""
+zakres zmiennych
+"""
+
+# zakres lokalny
+
+x = 5
+
+
+def wypisz1(a):
+    x = a
+    print(x)
+
+
+wypisz1(10)
+print(x)
+
+# zakres globalny
+
+y = 4
+
+
+def wypisz2(b):
+    global y
+    y = b
+    print(y)
+
+
+wypisz2(10)
+print(y)
+
+
+# zakres nonlocal
+
+def funkcja1():
+    a = 5
+    print(x)
+
+    def funkcja2():
+        nonlocal a
+        a = 10
+        print(x)
+
+    funkcja2()
+    print(a)
+
+
+funkcja1()
+
+"""
+wyrazenia lambda
+"""
+
+l = lambda a, b, c: a + b + c
+print(l(1, 2, 3))
+
+lista = [(lambda a: a + 2), (lambda a: a + 4), (lambda a: a * 8)]
+
+for i in lista:
+    print(i(2))
+
+print(lista[1](3))
+
+"""
+generator
+"""
+
+
+def generator(a):
+    for i in range(a):
+        yield i + 1
+for i in generator(4):
+    print(i, end=" ")
+
+
