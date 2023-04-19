38e5e178fa3494aae9952d52c1d2522483836ed5
Iza <lilamovie@wp.pl>
2023-04-19 09:04:02 +0200

    lab05

---
+++ Lab07/script1.py
@@ -0,0 +1,22 @@
+"""
+Lab07 - Klasy
+"""
+
+class Zwierz:
+    """Pierwsza klasa"""
+
+    def podaj_gatunek(selfself):
+        print("lis")
+
+a = Zwierz()
+print(a)
+
+b = Zwierz()
+print(b)
+
+# sprawdzenie, czy mamy do czynienia z tym samym obiektem
+print(a == b)
+
+# wywołanie metod obiektu
+a.podaj_gatunek()
+b.podaj_gatunek()
---
+++ Lab07/script2.py
@@ -0,0 +1,16 @@
+"""
+Lab07 - inicjalizowanie klas i atrybuty klas
+"""
+
+class Zwierz:
+    """Pierwsza klasa"""
+    rodzaj = "Zwierze"
+
+    def ___init___(self, gatunek, wiek):
+        self.gatunek = gatunek
+        self.wiek = wiek
+
+
+    def podaj_gatunek(selfself):
+        print("lis")
+
