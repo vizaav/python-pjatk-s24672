b8a702c94a252e3b5b108cb98924e7ad1c42500c
Iza <lilamovie@wp.pl>
2023-05-10 09:44:13 +0200

    lab09

---
+++ Lab09/zad1.py
@@ -0,0 +1,5 @@
+"""
+Zmodyfikuj zadanie dotyczące prostęj bazy danych produktów w taki sposób aby
+-dane produktów były zapisywane i odczytywane z pliku
+-zaimplementuj obsługę wyjątków wszędzie tam gdzie jest to potrzebne
+"""
---
+++ Lab09/zas2.py
@@ -0,0 +1,8 @@
+"""
+Napisz skrypt do obsługi struktury folderów.Skrypt powinien korzystać z
+metod obsługiwanych w systemie Windows i Linux.Użytkownik podaje nazwy folderów,
+ a skrypt tworzy strukturę.Skrypt powinien pozwolić na usuwanie i tworzenie
+ struktur.Wykorzystaj wiedzę nabytą od początku zajęć.Zastosuj klasy, funkcje
+  itp.jeśli␣↪jest to zasadne.Wybór uzasadnij.Miej na uwadze komfort obsługi
+   Twojego rozwiązania.
+"""
